a67896b37f3e54065e6a6cb7acc929a7bd50d78b
ibrahimabdullahibare <ibrahimabdullahibare@gail.com>
2024-11-03 23:57:37 -0900

    python-p3-cartoon-collections-lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    return [call.capitalize() + "!" for call in calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(items):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheese_types:
+            return item
+    return None
+
